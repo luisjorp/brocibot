ca0e200f66f86e8dee7b071ce185a0481a64e87b
LuisJo Ruano <luis.jose@arcadalabs.com>
2023-05-02 22:27:54 -0600

    ADD: server to preserve the context

--- server/server.js
+++ server/server.js
@@ -15,36 +15,42 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Define a function to generate a response given an input prompt
+async function generateResponse(prompt) {
+    // Generate a completion using the given prompt
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.5,
+        max_tokens: 600,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+    });
+
+    // Return the generated response text
+    return response.data.choices[0].text;
+}
+
 app.get('/', (req, res) => {
     res.status(200).send({
         message: 'Hello from BrociBot',
     });
 });
 
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
     try {
+        // Retrieve the input text from the request body
         const prompt = req.body.prompt;
-        const response = await openai.createCompletion({
-            /* The name of the model that we are using. */
-            model: "text-davinci-003",
-            /* A template literal. */
-            prompt: `${prompt}`,
-            /* A parameter that controls the randomness of the bot's response. */
-            temperature: 0,
-            /* Limiting the number of tokens that the bot can generate. */
-            max_tokens: 300,
-            /* A parameter that controls the randomness of the bot's response. */
-            top_p: 1,
-            /* A parameter that controls the randomness of the bot's response. */
-            frequency_penalty: 0.5,
-            /* A parameter that controls the randomness of the bot's response. */
-            presence_penalty: 0,
-        });
+
+        const responseText = await generateResponse(prompt);
+
+        // Send the response text back to the client
         res.status(200).send({
-            bot: response.data.choices[0].text,
+            bot: responseText,
         });
-    } catch (e) {
-        console.error(e);
+    } catch (error) {
+        console.error(error);
         res.status(500).send({
             error: e,
         });
